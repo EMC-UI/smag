d734810245ff3364447a33f6141378effee431ce
Greg Smith <gregory.p.smith@emc.com>
2016-08-29 16:50:41 -0600

    Remove ui.layout module for now

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 (function(angular, _, moment) {
   'use strict';
 
-  angular.module('app', ['ui.layout']).controller('controller', ['$scope','$interval', function($scope, $interval) {
+  angular.module('app', []).controller('controller', ['$scope','$interval', function($scope, $interval) {
     console.log('Got here!: ' + $scope);
   }]);
 
