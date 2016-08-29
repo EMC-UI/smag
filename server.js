be1f5868a83d5df10f537ed66a8fc32618baa0e1
Sarah Monzyk <smonzyk@hotmail.com>
2016-08-29 12:59:53 -0600

    Adding server.js

---
+++ server.js
@@ -0,0 +1,29 @@
+/**
+ * Created by monzys on 8/29/2016.
+ */
+#!/usr/bin/env node
+'use strict';
+
+var express = require('express');
+var restClient = require('request-promise');
+
+
+module.exports = (function() {
+
+    var port = 5000;
+
+    var init = function() {
+        var app = express();
+        app.use(express.static('.'));
+
+        //app.post('/auth', handleAuth)
+        //app.get('/tweets', getTweets)
+
+        console.log(`listening on ${port}`);
+        app.listen(port);
+    }
+
+    init();
+
+})();
+
