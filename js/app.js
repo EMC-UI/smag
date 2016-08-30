a6429a6a9fe1238f8a042e6ab2b668b5ab786beb
Kondakalla, Avinash <avinash.kondakalla@emc.com>
2016-08-30 09:12:38 -0600

    simple-SSH module

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 (function (angular) {
     "use strict";
     var app = angular.module("app", []);
+//    var SSH = require('simple-ssh');
 
     app.controller('controller', ['$scope','$interval', function($scope, $interval) {
         console.log('Got here!: ' + $scope);
     }]);
 
 
-    app.controller("ProjectListCtrl", ["$scope", "$location", '$http',
-        function ($scope, $location, $http) {
+    app.controller("ProjectListCtrl", ["$scope", "$location", '$http', function ($scope, $location, $http) {
             $http.get('/api/projects').
                 success(function(projects) {
                     $scope.projects = projects;
@@ -26,8 +26,22 @@
                 //TODO:: Disconnect the project session
                 //Hide the frame
                 $scope.selectedProject = undefined;
-            }
-        }
-    ]);
+            };
+//
+//            var sshExecCommand = function(project) {
+//                var ssh = new SSH({
+//                    host: 'localhost', //vma-avi.devlsvl.com
+//                    user: 'username', // vi-admin
+//                    pass: 'password' // ?Strong1?
+//                });
+//
+//                ssh.exec('echo $PATH', {
+//                    out: function(stdout) {
+//                        console.log(stdout);
+//                    }
+//                }).start();
+//            };
+
+        }]);
 
 }(window.angular));
