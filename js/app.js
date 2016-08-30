104e2822adbf8394c48b4fe8a294a0c34b1ccc7e
Kondakalla, Avinash <avinash.kondakalla@emc.com>
2016-08-29 22:58:22 -0600

    For debug: remove min file and add angular.js Update the UI with left hand navigation with project list Add iFrame with show and hide div Update the iFrame URL once the project is selected.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,33 @@
-(function(angular, _, moment) {
-  'use strict';
+(function (angular) {
+    "use strict";
+    var app = angular.module("app", []);
 
-  angular.module('app', []).controller('controller', ['$scope','$interval', function($scope, $interval) {
-    console.log('Got here!: ' + $scope);
-  }]);
+    app.controller('controller', ['$scope','$interval', function($scope, $interval) {
+        console.log('Got here!: ' + $scope);
+    }]);
 
-})(window.angular, window._, window.moment);
+
+    app.controller("ProjectListCtrl", ["$scope", "$location", '$http',
+        function ($scope, $location, $http) {
+            $http.get('/api/projects').
+                success(function(projects) {
+                    $scope.projects = projects;
+                });
+
+            $scope.select = function (project) {
+                // TODO:: Generate the URL and update the scope
+                //$scope.detailFrameSrc = '';
+                // Set the project to display
+                // TODO:: Need animation
+                $scope.selectedProject = project;
+            };
+
+            $scope.cancel = function(project) {
+                //TODO:: Disconnect the project session
+                //Hide the frame
+                $scope.selectedProject = undefined;
+            }
+        }
+    ]);
+
+}(window.angular));
